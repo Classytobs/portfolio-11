657218148cdbd8d66f2de1bf7a396903bf167187
classytobs <adewumitobi20@gmail.com>
2023-01-10 22:34:02 +0100

    working on the screen responsiveness

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
   const {theme, changeTheme}=useContext(ThemeContext);
 
   return (
-    <div className=' flex flex-col bg-zinc-900 gap-y-5 justify-center content-center py-8 min-h-screen max-w-full items-center'>
+    <div className=' flex flex-col bg-zinc-900 gap-y-6 justify-start content-center py-8 min-h-screen max-w-full items-center'>
       <div className='text-white p-2 cursor-pointer' onClick={changeTheme}>
           <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" /></svg>
