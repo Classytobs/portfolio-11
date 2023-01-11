81942d3d68ed658b78bbe6fb0da98f87df85ede9
classytobs <adewumitobi20@gmail.com>
2023-01-11 20:57:00 +0100

    adjusting the design and responsiveness of different components

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -7,15 +7,15 @@ export default function Home() {
   const {theme, changeTheme}=useContext(ThemeContext);
 
   return (
-    <div className=' flex flex-col bg-zinc-900 gap-y-6 justify-evenly content-center py-8 min-h-screen max-w-full items-center'>
+    <div className=' flex flex-col bg-zinc-900 gap-y-6 justify-start content-center py-8 min-h-screen max-w-full items-center'>
       <div className='text-white p-2 cursor-pointer' onClick={changeTheme}>
           <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" /></svg>
       </div>
       <div className='flex flex-col justify-between content-center items-center gap-y-3'>
-            <div className='w-52 h-64 flex justify-center items-center cursor-pointer 
-            transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 mb-3'>
-              <img src={img0} alt='me'className='h-56 w-56 rounded-full '/>
+            <div className='flex justify-center items-center cursor-pointer 
+            transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 w-72 h-72 duration-300 md:h-80 md:w-80 rounded-full mb-3'>
+              <img src={img0} alt='me'className='h-full w-full rounded-full'/>
             </div>
           
           <div className=' flex flex-col text-white px-4 justify-between content-center items-center text-3xl font-serif text-center'>
