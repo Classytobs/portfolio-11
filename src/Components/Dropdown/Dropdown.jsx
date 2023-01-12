c339c5173f7bea0c2a9bfc02bdf304f2ee1b5c97
classytobs <adewumitobi20@gmail.com>
2023-01-12 16:59:11 +0100

    adjusting the design and responsiveness of different components

--- src/Components/Dropdown/Dropdown.jsx
+++ src/Components/Dropdown/Dropdown.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Dropdown({isOpen, toggle}) {
   return (
-    <div className= {isOpen?'flex flex-col mt-20 text-center items-center bg-black h-screen text-white font-serif text-2xl gap-y-8 pt-6' : 'hidden'}
+    <div className= {isOpen?'flex fixed w-full flex-col mt-20 text-center items-center bg-black h-screen text-white font-serif text-2xl gap-y-8 pt-6' : 'hidden'}
     onClick={toggle}>
         <a href="#home">Home</a>
         <a href="#about">About</a>
