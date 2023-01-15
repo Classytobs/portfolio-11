7d3fc1a7840e2e8259d19442c77e8e2d10923666
classytobs <adewumitobi20@gmail.com>
2023-01-15 15:02:20 +0100

    implemented black and white theme using useContext

--- src/Components/Dropdown/Dropdown.jsx
+++ src/Components/Dropdown/Dropdown.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import { ThemeContext } from '../Context/Theme-context'
 
 export default function Dropdown({isOpen, toggle}) {
+  const {theme, changeTheme} = useContext(ThemeContext);
   return (
-    <div className= {isOpen?'flex fixed w-full flex-col mt-20 text-center items-center bg-black h-screen text-white font-serif text-2xl gap-y-8 pt-6' : 'hidden'}
-    onClick={toggle}>
-        <a href="#home">Home</a>
-        <a href="#about">About</a>
-        <a href="#pr">Portfolio</a>
-        <a href="#contact">Contact</a>   
-    </div>
+    <div className= {isOpen?'show' : 'hidden'}>
+    <div className={theme?'flex fixed w-full flex-col mt-20 text-center items-center h-screen font-serif text-2xl gap-y-8 pt-6 bg-black text-white':'flex fixed w-full flex-col mt-20 text-center items-center h-screen font-serif text-2xl gap-y-8 pt-6 bg-white text-black'}
+          onClick={toggle}> 
+          <a href="#home">Home</a> 
+          <a href="#about">About</a> 
+          <a href="#pr">Portfolio</a> 
+          <a href="#contact">Contact</a>  
+      </div>  
+    </div> 
   )
 }
