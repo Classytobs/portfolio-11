4772b4f00fefcec26edbf96cf492065fcc0c539e
classytobs <adewumitobi20@gmail.com>
2023-08-07 20:57:24 +0100

    working on screen responsiveness

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,11 +7,11 @@ export default function Footer() {
   const {theme}=useContext(ThemeContext);
 
   return (
-    <div className={theme? 'flex flex-col w-full justify-between items-center content-center px-32 gap-y-8 py-5 bg-slate-200 text-dark font-serif':
-    'flex flex-col w-full justify-between items-center bg-slate-900 gap-y-8 content-center px-32 py-5 font-serif text-white'}>
-        <div className='flex flex-row w-full justify-between items-center content-center'>
+    <div className={theme? 'flex flex-col w-full justify-between items-center content-center px-6 md:px-16 lg:px-32 gap-y-8 py-5 bg-slate-200 text-dark font-serif':
+    'flex flex-col w-full justify-between items-center bg-slate-900 gap-y-8 content-center px-6 md:px-16 lg:px-32 py-5 font-serif text-white'}>
+        <div className='flex flex-col md:flex-row w-full justify-between items-center content-center gap-y-8'>
 
-          <div href="#home"className='text-4xl cursor-pointer flex flex-row'>
+          <div href="#home"className='text-4xl cursor-pointer flex flex-row '>
             <div>Classy</div>
             <div className="text-blue-600">Tobs</div>
           </div>
