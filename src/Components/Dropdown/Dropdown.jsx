80f01dcfc01f4cc37500baa7f4f2c86f501338cf
classytobs <adewumitobi20@gmail.com>
2023-08-07 21:03:32 +0100

    working on screen responsiveness

--- src/Components/Dropdown/Dropdown.jsx
+++ src/Components/Dropdown/Dropdown.jsx
@@ -5,7 +5,7 @@ export default function Dropdown({isOpen, toggle}) {
   const {theme} = useContext(ThemeContext);
   return (
     <div className= {isOpen?'show' : 'hidden'}>
-    <div className={theme?'flex fixed w-full flex-col mt-20 text-center items-center h-screen font-serif text-2xl gap-y-8 pt-6 bg-black text-white':'flex fixed w-full flex-col mt-20 text-center items-center h-screen font-serif text-2xl gap-y-8 pt-6 bg-white text-black'}
+    <div className={theme?'flex fixed w-full flex-col mt-20 text-center items-center h-screen font-serif text-2xl gap-y-8 pt-6 bg-slate-100 text-black':'flex fixed w-full flex-col mt-20 text-center items-center h-screen font-serif text-2xl gap-y-8 pt-6 bg-slate-800 text-white'}
           onClick={toggle}> 
           <a href="#home">Home</a> 
           <a href="#about">About</a> 
